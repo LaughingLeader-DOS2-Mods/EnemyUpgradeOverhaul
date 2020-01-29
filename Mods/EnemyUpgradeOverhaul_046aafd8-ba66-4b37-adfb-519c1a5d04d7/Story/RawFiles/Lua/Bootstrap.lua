dc8a7a46a3502ff22ec51a8f3c19a80c8ddb3bd8
LaughingLeader <laughingleader@gmail.com>
2020-01-28 19:26:46 -0600

    Added some experimental code for clearing gain values

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Bootstrap.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Bootstrap.lua
@@ -109,4 +109,20 @@ else
     Ext.Print("[LLENEMY:Bootstrap.lua] [*WARNING*] Extender version is less than v36! Stat overrides ain't happenin', chief.")
 end
 
+function LLENEMY_Ext_ClearGain(char)
+	local stats = nil
+	if NRD_GetVersion() >= 39 then
+		stats = NRD_CharacterGetStatString(char)
+	end
+	if stats == nil then stats = GetStatString(char) end
+	if stats ~= nil then
+		local gain = NRD_StatGetInt(stats, "Gain")
+		gain = gain - 1
+		Osi.LeaderLog_LogInt("DEBUG", "[LLENEMY:Bootstrap.lua:LLENEMY_Ext_ClearGain] Removing " .. tostring(gain) .." from ("..tostring(char)..").");
+		NRD_CharacterSetPermanentBoostInt(char, "Gain", gain);
+		NRD_CharacterSetPermanentBoostInt(char, "Gain", 1);
+		CharacterAddAttribute(char, "Strength", 0);
+	end
+end
+
 Ext.Print("[LLENEMY:Bootstrap.lua] Finished running.")
