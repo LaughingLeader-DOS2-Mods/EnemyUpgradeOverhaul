159b426f9f8030da278e6e8b3bebb8efcd2c93cb
LaughingLeader <laughingleader@gmail.com>
2020-02-02 13:03:29 -0600

    Tweaked debug scripts for testing stats applying via behavior

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Bootstrap.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Bootstrap.lua
@@ -90,6 +90,7 @@ local function OverrideStats()
 end
 
 Ext.Require("EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7", "LLENEMY_BonusSkills.lua")
+Ext.Require("EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7", "LLENEMY_Debug.lua")
 
 local ModuleLoad = function ()
     Ext.Print("[LLENEMY:Bootstrap.lua] Module is loading.")
---
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/LLENEMY_Debug.lua
@@ -0,0 +1,67 @@
+local character_stats = {
+	"CurrentVitality",
+	"CurrentArmor",
+	"CurrentMagicArmor",
+	"ArmorAfterHitCooldownMultiplier",
+	"MagicArmorAfterHitCooldownMultiplier",
+	"CurrentAP",
+	"BonusActionPoints",
+	"Experience",
+	"Reputation",
+	"Flanked",
+	"Karma",
+	"MaxVitality",
+	"BaseMaxVitality",
+	"MaxArmor",
+	"BaseMaxArmor",
+	"MaxMagicArmor",
+	"BaseMaxMagicArmor",
+	"Sight",
+	"BaseSight",
+	"MaxSummons",
+	"BaseMaxSummons"
+}
+
+local character_stats_computed = {
+	"MaxMp",
+	"APStart",
+	"APRecovery",
+	"APMaximum",
+	"Strength",
+	"Finesse",
+	"Intelligence",
+	"Vitality",
+	"Memory",
+	"Wits",
+	"Accuracy",
+	"Dodge",
+	"CriticalChance",
+	"FireResistance",
+	"EarthResistance",
+	"WaterResistance",
+	"AirResistance",
+	"PoisonResistance",
+	"ShadowResistance",
+	"CustomResistance",
+	"LifeSteal",
+	"Sight",
+	"Hearing",
+	"Movement",
+	"Initiative",
+	"ChanceToHitBoost"
+}
+
+function LLENEMY_Ext_TraceCharacterStats(char)
+	Ext.Print("====== Stats: "..char.." ======")
+	Ext.Print("==== COMPUTED ====")
+	for _,stat in pairs(character_stats_computed) do
+		local base = NRD_CharacterGetComputedStat(char, stat, 1)
+		local current = NRD_CharacterGetComputedStat(char, stat, 0)
+		Ext.Print(stat..": "..tostring(current).."("..tostring(base)..")")
+	end
+	Ext.Print("==== REGULAR ====")
+	for _,stat in pairs(character_stats) do
+		local val = NRD_CharacterGetStatInt(char, stat)
+		Ext.Print(stat..": "..tostring(val))
+	end
+end
