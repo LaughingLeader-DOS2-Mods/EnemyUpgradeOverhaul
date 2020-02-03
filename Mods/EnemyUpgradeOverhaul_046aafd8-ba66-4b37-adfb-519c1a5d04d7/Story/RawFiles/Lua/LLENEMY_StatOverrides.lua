ae909bac0199699a06e3f1597fc0a78d29951c4b
LaughingLeader <laughingleader@gmail.com>
2020-02-02 20:03:09 -0600

    Moved stat overrides to a new script

---
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/LLENEMY_StatOverrides.lua
@@ -0,0 +1,105 @@
+local stat_overrides = {
+	_NpcDaggers = {
+		Talents = "ViolentMagic;Ambidextrous"
+	},
+	_NpcUnarmed = {
+		Talents = "ViolentMagic;Ambidextrous"
+	},
+	_NpcWands_Water = {
+		Talents = "ViolentMagic;Ambidextrous"
+	},
+	_NpcSwords = {
+		Talents = "ViolentMagic;Ambidextrous"
+	},
+	_NpcAxes = {
+		Talents = "ViolentMagic;Ambidextrous"
+	},
+	_NpcClubs = {
+		Talents = "ViolentMagic;Ambidextrous"
+	},
+	_NpcTwoHandedSwords = {
+		Talents = "ViolentMagic"
+	},
+	_NpcTwoHandedAxes = {
+		Talents = "ViolentMagic"
+	},
+	_NpcTwoHandedMaces = {
+		Talents = "ViolentMagic"
+	},
+	_NpcSpears = {
+		Talents = "ViolentMagic"
+	},
+	_NpcStaffs = {
+		Talents = "ViolentMagic;FaroutDude"
+	},
+	_NpcStaffs_Fire = {
+		Talents = "ViolentMagic;FaroutDude"
+	},
+	_NpcStaffs_Water = {
+		Talents = "ViolentMagic;FaroutDude"
+	},
+	_NpcStaffs_Poison = {
+		Talents = "ViolentMagic;FaroutDude"
+	},
+	_NpcStaffs_Earth = {
+		Talents = "ViolentMagic;FaroutDude"
+	},
+	_NpcStaffs_Air = {
+		Talents = "ViolentMagic;FaroutDude"
+	},
+	_NpcBows = {
+		Talents = "ViolentMagic;ElementalRanger"
+	},
+	_NpcCrossbows = {
+		Talents = "ViolentMagic;ElementalRanger"
+	},
+	_NpcWands_Fire = {
+		Talents = "ViolentMagic;Ambidextrous"
+	},
+	_NpcWands_Air = {
+		Talents = "ViolentMagic;Ambidextrous"
+	},
+	_NpcWands_Poison = {
+		Talents = "ViolentMagic;Ambidextrous"
+	},
+	LLENEMY_TALENT_BULLY_EXTENDER = {
+		Description = "LLENEMY_TALENT_BULLY_EXTENDER_Description"
+	}
+}
+
+local function OverrideStats()
+    local total_changes = 0
+    local total_stats = 0
+
+    local debug_print = false
+
+    --LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c
+    if _G["LeaderLib"] ~= nil or Ext.IsModLoaded("7e737d2f-31d2-4751-963f-be6ccc59cd0c") then
+        if _G["LeaderLib_Lua_PrintEnabled"] == true then
+            debug_print = true
+        end
+    end
+
+    for statname,overrides in pairs(stat_overrides) do
+        for property,value in pairs(overrides) do
+            if debug_print then Ext.Print("[LLENEMY:Bootstrap.lua] Overriding stat: " .. statname .. " (".. property ..") = \"".. value .."\"") end
+            Ext.StatSetAttribute(statname, property, value)
+            total_changes = total_changes + 1
+        end
+        total_stats = total_stats + 1
+	end
+	
+    Ext.Print("[LLENEMY:Bootstrap.lua] Changed ("..tostring(total_changes)..") properties in ("..tostring(total_stats)..") stats (added talents to enemy weapons).")
+end
+
+local function ModuleLoad()
+    Ext.Print("[LLENEMY:Bootstrap.lua] Module is loading.")
+	OverrideStats()
+end
+
+--v36 and higher
+if Ext.RegisterListener ~= nil then
+    Ext.RegisterListener("ModuleLoading", ModuleLoad)
+else
+    Ext.Print("[LLENEMY:Bootstrap.lua] [*WARNING*] Extender version is less than v36! Stat overrides ain't happenin', chief.")
+end
