6a2fbdbfb691477c8d33eeec14fdd3d4daf484c4
LaughingLeader <laughingleader@gmail.com>
2020-02-07 16:27:36 -0600

    Tweaking description param test

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/LLENEMY_DescriptionParams.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/LLENEMY_DescriptionParams.lua
@@ -3,11 +3,20 @@ local upgrade_info = {}
 
 function LLENEMY_EXT_StoreUpgradeInfo(uuid, str)
 	upgrade_info[uuid] = str
+	Ext.Print("Stored upgrade_info: " .. uuid .. " = " .. str)
+end
+
+function LLENEMY_EXT_RemoveUpgradeInfo(uuid)
+	upgrade_info[uuid] = nil;
+	Ext.Print("Removed upgrade_info entry for " .. uuid)
 end
 
 local function StatusGetDescriptionParam(status, statusSource, character, param)
+	Ext.Print("[LLENEMY_DescriptionParams.lua] Getting params for (".. tostring(status) ..") Character (".. tostring(character.MyGuid) ..") param ("..tostring(param)..") Source ("..tostring(statusSource.MyGuid)..").")
+	
 	if status == "LLENEMY_UPGRADE_INFO" then
 		if param == "UpgradeInfo" then
+			Ext.Print("Getting upgrade_info for " .. tostring(character.MyGuid))
 			local info_str = upgrade_info[character.MyGuid]
 			if info_str ~= nil then
 				return info_str
