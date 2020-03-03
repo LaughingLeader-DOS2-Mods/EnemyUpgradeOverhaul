1233c07c7095f121c33a9bbdc9d50afa1a41fccd
LaughingLeader <laughingleader@gmail.com>
2020-03-03 09:01:14 -0600

    Moved extender mugging to lua

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/LLENEMY_GameMechanics.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/LLENEMY_GameMechanics.lua
@@ -47,3 +47,59 @@ function LLENEMY_Ext_IncreaseRage(character, damage, handle)
 	Osi.LeaderLib_Variables_DB_ModifyVariableInt(character, "LLENEMY_Rage", add_rage, 999, 0);
 	Ext.Print("[LLENEMY_GameMechanics.lua:LLENEMY_Ext_IncreaseRage] Added (",add_rage,") Rage to (",character,").")
 end
+
+function LLENEMY_Ext_MugTarget_Start(character, target, handle)
+	local hit_type = NRD_StatusGetInt(target, handle, "HitReason")
+	Ext.Print("[LLENEMY_GameMechanics.lua:LLENEMY_Ext_MugTarget_Start] Hit type: " .. tostring(hit_type))
+	if (hit_type == 0 or hit_type == 3) and LeaderLib.Game.HitSucceeded(handle, target) then
+		Ext.Print("[LLENEMY_GameMechanics.lua:LLENEMY_Ext_MugTarget_Start] ("..tostring(character)..") is mugging target: ", target)
+		Osi.LLENEMY_Talents_MugTarget(character, target)
+	else
+		Ext.Print("[LLENEMY_GameMechanics.lua:LLENEMY_Ext_MugTarget_Start] Dodged: ",NRD_StatusGetInt(target, handle, "Dodged")," | Missed: ", NRD_StatusGetInt(target, handle, "Missed")," | Blocked: ",NRD_StatusGetInt(target, handle, "Blocked"))
+	end
+end
+
+function LLENEMY_Ext_MugTarget_StealGold(character, target)
+	local gold = CharacterGetGold(target)
+	Ext.Print("[LLENEMY_GameMechanics.lua:LLENEMY_Ext_MugTarget_StealGold] Target ("..tostring(target)..") has ("..tostring(gold)..") gold.")
+	if gold > 0 then
+		local add_gold = math.tointeger(math.max(math.ceil(gold / 8), 1))
+		local remove_gold = math.tointeger(add_gold * -1)
+		CharacterAddGold(target, remove_gold)
+		CharacterAddGold(character, add_gold)
+		--CharacterDisplayTextWithParam(target, "LLENEMY_DisplayText_MuggedGold_Target", add_gold);
+		CharacterDisplayTextWithParam(character, "LLENEMY_DisplayText_MuggedGold_Enemy", add_gold);
+		return add_gold
+	end
+	return 0
+end
+
+function LLENEMY_Ext_MugTarget_End(character, target)
+	local items = Osi.DB_LLENEMY_Talents_Temp_MasterThief_Items:Get(target, nil, nil)
+	Ext.Print("[LLENEMY_GameMechanics.lua:LLENEMY_Ext_MugTarget_End] Picking items from:\n",LeaderLib.Common.Dump(items))
+	local item_entry = LeaderLib.Common.GetRandomTableEntry(items)	
+	if item_entry ~= nil then
+		local item = item_entry[3]
+		Ext.Print("[LLENEMY_GameMechanics.lua:LLENEMY_Ext_MugTarget_End] Transfering (",item,") from (",target,") to (",character,").")
+		ItemToInventory(item, character, 1, CharacterIsPlayer(character), 0)
+		Osi.LLENEMY_Talents_OnTargetMugged(character, target, item, 1)
+		LLENEMY_Ext_MugTarget_DisplayText(character, target, item, 1)
+	end
+	Osi.LLENEMY_Talents_Internal_Mug_ClearData(target)
+end
+
+function LLENEMY_Ext_MugTarget_DisplayText(character, target, item, amount)
+	local lost_gold = LLENEMY_Ext_MugTarget_StealGold(character, target)
+	local template = GetTemplate(String(item))
+	local templateGUID = NRD_GuidString(template)
+	local handle,name = ItemTemplateGetDisplayString(templateGUID)
+	local text = ""
+	if amount > 1 then
+		text = "<font color='#FF3333' size='23'><font color='#FE6E27' size='26'>"..tostring(name).."</font> x"..tostring(amount).." was stolen!</font>"
+	else
+		text = "<font color='#FF3333' size='23'><font color='#FE6E27' size='26'>"..tostring(name).."</font> was stolen!</font>"
+	end
+	CharacterStatusText(target, text)
+	if lost_gold > 0 then CharacterStatusText(target, "<font color='#ffbd30' size='23'>Lost "..tostring(lost_gold).." Gold!</font>") end
+end
+--Ext.NewCall(LLENEMY_Ext_MugTarget_DisplayText, "LLENEMY_Ext_MugTarget_DisplayText", "(CHARACTERGUID)_Enemy, (CHARACTERGUID)_Target, (ITEMGUID)_Item, (INTEGER)_Amount");
