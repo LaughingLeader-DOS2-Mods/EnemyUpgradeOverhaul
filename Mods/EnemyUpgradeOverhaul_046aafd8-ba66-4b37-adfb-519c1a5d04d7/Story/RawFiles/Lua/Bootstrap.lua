6e909dd57f091dc87e57d8aafcabc646873e0e8e
LaughingLeader <laughingleader@gmail.com>
2020-03-23 23:10:01 -0500

    Check the extender version before loading client/server for older extender versions

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Bootstrap.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Bootstrap.lua
@@ -1,6 +1,7 @@
 
 
-Ext.Require("EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7", "BootstrapClient.lua")
-Ext.Require("EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7", "BootstrapServer.lua")
-
-Ext.Print("[LLENEMY:Bootstrap.lua] Finished running.")
+if Ext.Version() < 42 then
+	Ext.Print("[LLENEMY:Bootstrap.lua] Loading both server/client scripts for old extender version (" .. tostring(Ext.Version()) .. ").")
+	Ext.Require("EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7", "BootstrapClient.lua")
+	Ext.Require("EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7", "BootstrapServer.lua")
+end
