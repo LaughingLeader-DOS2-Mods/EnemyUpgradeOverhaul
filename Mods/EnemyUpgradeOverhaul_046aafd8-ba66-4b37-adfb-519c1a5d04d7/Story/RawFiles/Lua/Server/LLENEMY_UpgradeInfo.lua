2658ddde4062b070a2b60f4fd37595fd5b74e00c
LaughingLeader <laughingleader@gmail.com>
2020-03-30 14:37:38 -0500

    Buffer upgrade info sending with a timer

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Server/LLENEMY_UpgradeInfo.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Server/LLENEMY_UpgradeInfo.lua
@@ -4,9 +4,12 @@ function LLENEMY_Ext_StoreUpgradeInfo(uuid, str)
 	if LeaderLib.Common.StringIsNullOrEmpty(uuid) == false and LeaderLib.Common.StringIsNullOrEmpty(str) == false then
 		EnemyUpgradeOverhaul.UpgradeInfo[uuid] = str
 		Ext.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Stored upgrade info entry for (" .. uuid .. ")")
-		if Ext.Version() >= 42 then
-			Ext.BroadcastMessage("LLENEMY_UpgradeInfo", Ext.JsonStringify(EnemyUpgradeOverhaul.UpgradeInfo), nil)
-		end
+	end
+end
+
+function LLENEMY_Ext_SendUpgradeInfo()
+	if Ext.Version() >= 42 then
+		Ext.BroadcastMessage("LLENEMY_UpgradeInfo", Ext.JsonStringify(EnemyUpgradeOverhaul.UpgradeInfo), nil)
 	end
 end
 
@@ -17,15 +20,3 @@ function LLENEMY_Ext_RemoveUpgradeInfo(uuid)
 		Ext.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Removed upgrade info entry for (" .. uuid .. ")")
 	end
 end
-
-function LLENEMY_Ext_TestNetMessage(channel, payload)
-	local host = CharacterGetHostCharacter()
-	Ext.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Sending message: [".. tostring(channel) .."]" .. tostring(payload))
-	Ext.BroadcastMessage(channel, payload, "09478f32-8fbf-4502-a59d-011e4d1b3d4d")
-end
-
-local function LLENEMY_PayloadTest(channel, payload)
-	Ext.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Received message: [".. tostring(channel) .."]" .. tostring(payload))
-end
-
-Ext.RegisterNetListener("LLENEMY_PayloadTest", LLENEMY_PayloadTest)
