9c5632a933588ce73767a341d14f7d72ccb4606b
LaughingLeader <laughingleader@gmail.com>
2020-04-05 05:24:51 -0500

    Copy upgrade info on duplicants

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Server/LLENEMY_UpgradeInfo.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Server/LLENEMY_UpgradeInfo.lua
@@ -3,11 +3,27 @@
 function LLENEMY_Ext_StoreUpgradeInfo(uuid, str)
 	if LeaderLib.Common.StringIsNullOrEmpty(uuid) == false and LeaderLib.Common.StringIsNullOrEmpty(str) == false then
 		local cp = GetVarInteger(uuid, "LLENEMY_ChallengePoints")
-		EnemyUpgradeOverhaul.UpgradeInfo[uuid] = {
+		local info = {
 			upgrades = str,
 			cp = cp,
 			isDuplicant = IsTagged(uuid, "LLENEMY_Duplicant") == 1
 		}
+		EnemyUpgradeOverhaul.UpgradeInfo[uuid] = info
+		local region = GetRegion(uuid)
+		local dupesTable = Osi.DB_LLENEMY_Duplication_Temp_Active:Get(uuid, nil, region)
+		if dupesTable ~= nil and #dupesTable > 0 then
+			for i,entry in pairs(dupesTable) do
+				local dupeUUID = entry[2]
+				if dupeUUID ~= nil and dupeUUID ~= "" then
+					local dupeTable = {
+						upgrades = str,
+						cp = cp,
+						isDuplicant = true
+					}
+					EnemyUpgradeOverhaul.UpgradeInfo[dupeUUID] = dupeTable
+				end
+			end
+		end
 		if Ext.IsDeveloperMode() then
 			Ext.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Stored upgrade info entry for (" .. uuid .. ")")
 		end
