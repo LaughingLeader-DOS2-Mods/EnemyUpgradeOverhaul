50b798b7271153572173c1591be2c397a037804d
LaughingLeader <laughingleader@gmail.com>
2020-04-07 19:12:54 -0500

    Testing UI creation

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/BootstrapClient.lua
@@ -8,3 +8,12 @@ end
 
 Ext.RegisterListener("ModuleLoading", LLENEMY_Client_ModuleLoading)
 --Ext.RegisterListener("ModuleResume", LLENEMY_Client_ModuleResume)
+
+local function LLENEMY_Client_SessionLoaded()
+	--local ui = Ext.CreateUI("LLENEMY_Test_SecondHotbar", "Public/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/GUI/hotBar.swf", 99)
+	--ui:Show()
+	--ui:SetPosition(100, 300)
+	--Ext.Print("================Created second hotbar?!=======================")
+end
+
+--Ext.RegisterListener("SessionLoaded", LLENEMY_Client_SessionLoaded)
