8ed69d1c9260e04f3b58566630ba3bc089bb3f86
LaughingLeader <laughingleader@gmail.com>
2020-04-08 21:48:42 -0500

    Reworked upgrade info building / storing

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Server/LLENEMY_UpgradeInfo.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Server/LLENEMY_UpgradeInfo.lua
@@ -1,36 +1,83 @@
 --EnemyUpgradeOverhaul["UpgradeInfo"] = {}
 
-function LLENEMY_Ext_StoreUpgradeInfo(uuid, str)
-	if LeaderLib.Common.StringIsNullOrEmpty(uuid) == false and LeaderLib.Common.StringIsNullOrEmpty(str) == false then
-		local cp = GetVarInteger(uuid, "LLENEMY_ChallengePoints")
-		local info = {
-			upgrades = str,
+local function GetUpgradeInfo(uuid)
+	local cp = GetVarInteger(uuid, "LLENEMY_ChallengePoints")
+	if cp == nil or cp < 0 then cp = 0 end
+	local info = EnemyUpgradeOverhaul.UpgradeInfo[uuid]
+	if info == nil then
+		info = {
+			upgrades = {},
 			cp = cp,
 			isDuplicant = IsTagged(uuid, "LLENEMY_Duplicant") == 1
 		}
-		EnemyUpgradeOverhaul.UpgradeInfo[uuid] = info
-		local region = GetRegion(uuid)
-		local dupesTable = Osi.DB_LLENEMY_Duplication_Temp_Active:Get(uuid, nil, region)
-		if dupesTable ~= nil and #dupesTable > 0 then
-			for i,entry in pairs(dupesTable) do
-				local dupeUUID = entry[2]
-				if dupeUUID ~= nil and dupeUUID ~= "" then
-					local dupeTable = {
-						upgrades = str,
-						cp = cp,
-						isDuplicant = true
-					}
-					EnemyUpgradeOverhaul.UpgradeInfo[dupeUUID] = dupeTable
-				end
-			end
+	else
+		info.cp = cp
+	end
+	return info
+end
+
+function LLENEMY_Ext_UpgradeInfo_ResolveStatus(uuid, info)
+	if info == nil then
+		info = GetUpgradeInfo[uuid]
+	end
+	if info ~= nil and #info.upgrades > 0 then
+		if HasActiveStatus(uuid, "LLENEMY_UPGRADE_INFO") == 0 then
+			ApplyStatus(uuid, "LLENEMY_UPGRADE_INFO", -1.0, 0, uuid);
 		end
-		if Ext.IsDeveloperMode() then
-			Ext.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Stored upgrade info entry for (" .. uuid .. ")")
+	elseif HasActiveStatus(uuid, "LLENEMY_UPGRADE_INFO") == 1 then
+		EnemyUpgradeOverhaul.UpgradeInfo[uuid] = nil
+		RemoveStatus(uuid, "LLENEMY_UPGRADE_INFO")
+	end
+end
+
+function LLENEMY_Ext_UpgradeInfo_CreateInfoString(uuid, info)
+	Osi.DB_LLENEMY_UpgradeInfo_Temp_InfoString:Delete(uuid, nil)
+	if info == nil then
+		info = GetUpgradeInfo[uuid]
+	end
+	if info ~= nil and #info.upgrades > 0 then
+		local infoStr = LeaderLib.Common.StringJoin(";",info.upgrades)
+		if infoStr ~= "" and infoStr ~= nil then
+			Osi.DB_LLENEMY_UpgradeInfo_Temp_InfoString(uuid, infoStr)
+			LeaderLib.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Saved upgrade info string for ("..uuid.."):("..infoStr..").")
 		end
 	end
 end
 
-function LLENEMY_Ext_SendUpgradeInfo()
+function LLENEMY_Ext_UpgradeInfo_Build(uuid)
+	local info = GetUpgradeInfo[uuid]
+	for key,group in pairs(EnemyUpgradeOverhaul.UpgradeData) do
+		for i,status in pairs(group) do
+			if HasActiveStatus(uuid, status) == 1 then
+				info.upgrades[#info.upgrades+1] = status
+			end
+		end
+	end
+	if #info.upgrades > 0 then
+		LLENEMY_Ext_UpgradeInfo_CreateInfoString(uuid, info)
+		Osi.LLENEMY_UpgradeInfo_StartTimer("Timers_LLENEMY_UpgradeInfo_SendInfo", 50)
+	end
+	LLENEMY_Ext_UpgradeInfo_ResolveStatus(uuid, info)
+end
+
+function LLENEMY_Ext_OnUpgradeApplied(uuid, status)
+	local infoText = EnemyUpgradeOverhaul.UpgradeData.Statuses[status]
+	if infoText == nil then	infoText = EnemyUpgradeOverhaul.UpgradeData.ArmorBoostStatuses end
+	if infoText == nil then	infoText = EnemyUpgradeOverhaul.UpgradeData.MagicArmorBoostStatuses end
+	if infoText == nil then	infoText = EnemyUpgradeOverhaul.UpgradeData.DamageBoostStatuses end
+	if infoText == nil then	infoText = EnemyUpgradeOverhaul.UpgradeData.VitalityBoostStatuses end
+	if infoText ~= nil then
+		--Osi.DB_LLENEMY_UpgradeInfo_Temp_ActiveUpgrades(char, status)
+		local info = GetUpgradeInfo[uuid]
+		info.upgrades[#info.upgrades+1] = status
+		LLENEMY_Ext_UpgradeInfo_CreateInfoString(uuid, info)
+		LeaderLib.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Added upgrade ("..status..") to info for ("..uuid..").")
+		LLENEMY_Ext_UpgradeInfo_ResolveStatus(uuid, info)
+		Osi.LLENEMY_UpgradeInfo_StartTimer("Timers_LLENEMY_UpgradeInfo_SendInfo", 250)
+	end
+end
+
+function LLENEMY_Ext_UpgradeInfo_Send()
 	if Ext.IsDeveloperMode() then
 		Ext.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Sending update info to clients.")
 	end
@@ -39,12 +86,43 @@ function LLENEMY_Ext_SendUpgradeInfo()
 	end
 end
 
-function LLENEMY_Ext_RemoveUpgradeInfo(uuid)
+function LLENEMY_Ext_UpgradeInfo_Remove(uuid)
+	Osi.DB_LLENEMY_UpgradeInfo_Temp_InfoString:Delete(uuid, nil)
 	local info = EnemyUpgradeOverhaul.UpgradeInfo[uuid]
 	if info ~= nil then
 		EnemyUpgradeOverhaul.UpgradeInfo[uuid] = nil
 		if Ext.IsDeveloperMode() then
 			Ext.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Removed upgrade info entry for (" .. uuid .. ")")
+		end
+		if HasActiveStatus(uuid, "LLENEMY_UPGRADE_INFO") == 1 then
+			RemoveStatus(uuid, "LLENEMY_UPGRADE_INFO")
+		end
+	end
+end
+
+function LLENEMY_Ext_UpgradeInfo_LoadSavedInfo()
+	local saved = Osi.DB_LLENEMY_UpgradeInfo_Temp_InfoString:Get(nil, nil)
+	if saved ~= nil and #saved > 0 then
+		for i,entry in pairs(saved) do
+			local uuid = entry[1]
+			local infoStr = entry[2]
+			if infoStr ~= nil and infoStr ~= "" then
+				local info = GetUpgradeInfo[uuid]
+				local upgrades = LeaderLib.Common.StringSplit(";", infoStr)
+				if upgrades ~= nil and #upgrades > 0 then
+					info.upgrades = {}
+					for i,status in pairs(upgrades) do
+						if HasActiveStatus(uuid, status) == 1 then
+							info.upgrades[#info.upgrades+1] = status
+						end
+					end
+					LeaderLib.Print("[EnemyUpgradeOverhaul:LLENEMY_UpgradeInfo.lua] Loaded upgrade info string for ("..uuid.."):("..infoStr..").")
+					-- Re-create the info string since active statuses may have changed
+					LLENEMY_Ext_UpgradeInfo_CreateInfoString(uuid, info)
+				end
+			else
+				Osi.DB_LLENEMY_UpgradeInfo_Temp_InfoString:Delete(uuid, nil)
+			end
 		end
 	end
 end
