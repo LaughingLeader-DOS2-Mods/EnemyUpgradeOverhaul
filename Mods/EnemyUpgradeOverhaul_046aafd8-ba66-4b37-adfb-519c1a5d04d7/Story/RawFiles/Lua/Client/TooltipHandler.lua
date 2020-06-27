4068756e52e0e8853f5aed81226c8a83d6e6c198
LaughingLeader <laughingleader@gmail.com>
2020-06-27 18:17:05 -0500

    Added shadow item affix name support for armor (of Shadow, etc)

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Client/TooltipHandler.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Client/TooltipHandler.lua
@@ -2,6 +2,9 @@
 local TranslatedString = LeaderLib.Classes["TranslatedString"]
 
 local maxSummonsText = TranslatedString:Create("hd248998fge250g4a7bg8dd3gc88f19fbe5f6", "Maximum Summons")
+local ShadowItemFallbackDescription = "A <i>strange</i> item retrieved from a <font color='#9B30FF' face='Copperplate Gothic Bold'>Shadow Orb</font>.<br><font color='#BDA0CB'>Cold to the touch, when this item is held, your grip on reality may begin to slip.</font>"
+local ShadowItemDescription = TranslatedString:Create("h179efab0g7e6cg441ag8083gb11964394dc4", ShadowItemFallbackDescription)
+local ShadowItemNameAffix = TranslatedString:Create("h1d44d1a4g804bg43fbg80dfgd3e3d07a897d", "<font color='#A020F0'>[1] of Shadows</font>")
 
 ---@param item EsvItem
 ---@param tooltip TooltipData
@@ -22,6 +25,20 @@ local function OnItemTooltip(item, tooltip)
 					Value = maxSummons,
 				}
 				tooltip:AppendElement(element)
+			end
+			local element = tooltip:GetElement("ItemDescription")
+			if element ~= nil then
+				if not LeaderLib.StringHelpers.IsNullOrEmpty(element.Label) then
+					element.Label = element.Label .. "<br>" .. ShadowItemDescription.Value
+				else
+					element.Label = ShadowItemDescription.Value
+				end
+			end
+			if item.ItemType == "Armor" then
+				element = tooltip:GetElement("ItemName")
+				if element ~= nil then
+					element.Label = ShadowItemNameAffix.Value:gsub("%[1%]", element.Label:gsub("<font.->", ""):gsub("</font>.*", ""))
+				end
 			end
 		end
 	end
