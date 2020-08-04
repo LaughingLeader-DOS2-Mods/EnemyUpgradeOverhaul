edcd0cca359000cc998965dea7ff0cc5f2c79b23
LaughingLeader <laughingleader@gmail.com>
2020-08-04 02:45:23 -0500

    Tweaked bonus roll loop for it's exactly how many rolls were determined

--- Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Server/HardmodeMechanics.lua
+++ Mods/EnemyUpgradeOverhaul_046aafd8-ba66-4b37-adfb-519c1a5d04d7/Story/RawFiles/Lua/Server/HardmodeMechanics.lua
@@ -1,7 +1,12 @@
 function HM_RollAdditionalUpgrades(char)
-	local bonusRolls = Ext.Random(1,3) + 1
-	for i=bonusRolls,0,-1 do
-		Osi.LLENEMY_Upgrades_RollForUpgrades(char)
-		--Ext.Print("Rolling bonus roll: " .. tostring(i) .. " | " .. char)
+	local vars = Settings.Global.Variables
+	local min = vars.Hardmode_MinBonusRolls.Value or 1
+	local max = vars.Hardmode_MaxBonusRolls.Value or 4
+	local bonusRolls = Ext.Random(min, max)
+	if bonusRolls > 0 then
+		for i=bonusRolls,1,-1 do
+			Osi.LLENEMY_Upgrades_RollForUpgrades(char)
+			--Ext.Print("Rolling bonus roll: " .. tostring(i) .. " | " .. char)
+		end
 	end
 end
